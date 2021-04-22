b15f18c4ca8f7ba35bbe98cd77674c3c72d17e13
nathankendsersky <nate.kendsersky@gmail.com>
2021-04-22 19:19:40 -0400

    Bail and Statutue Categories

--- analyses/team3/nk_explore.R
+++ analyses/team3/nk_explore.R
@@ -3,14 +3,33 @@
 library(tidyverse)
 
 #### load data ####
+
+docket_charges <- readRDS("reports/data/clean/docket_charges.Rds")
+
+
+
+ggplot(docket_charges, aes())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#### backstop ####
+#### scraps ####
+
 off_16_17 <- read_csv("../../data/offenses_dispositions_v2_2016_2017.csv")
 off_18_19 <- read_csv("../../data/offenses_dispositions_v2_2018_2019.csv")
 table(off_18_19$grade)
-
-
-
-#### Q: Changes in disposition, min/max period, sentence type? ####
-
 
 standarize_min_period <- function(dataset) {
   
@@ -47,33 +66,3 @@ slimmer_min_period <- function(dataset,before_after) {
 
 off_16_17_slim <- slimmer_min_period(off_16_17,"before")
 off_18_19_slim <- slimmer_min_period(off_18_19,"after")
-
-off_min_period <- rbind(off_16_17_slim,off_18_19_slim)
-
-ggplot(off_min_period,aes(x=before_after_LK,y=min_period_in_month)) + 
-  geom_violin()
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-#### scraps ####
-
-# comparing grade from 16/17 to 18/19; different judges
-summary(off_16_17$grade)
-summary(off_18_19$grade)
-
-ggplot(off_16_17, aes(factor(grade))) +
-  geom_bar(stat="count", position = "dodge") + 
-  scale_fill_brewer(palette = "Set1")
-
-unique(off_16_17$disposing_authority__last_name)
